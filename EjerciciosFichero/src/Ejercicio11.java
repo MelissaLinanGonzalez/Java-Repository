781ba7c5fed18829022562089030b6f884ba1267
Melissa Liñán <melissa16800@gmail.com>
2025-03-25 12:25:07 +0100

    Ejercicio 11 File

---
+++ EjerciciosFichero/src/Ejercicio11.java
@@ -0,0 +1,22 @@
+//Listar archivos en un directorio
+//Crea un programa que muestre todos los archivos y carpetas dentro del directorio actual, indicando si cada uno es archivo o directorio
+
+import java.io.File;
+
+public class Ejercicio11 {
+    public static void main(String[] args) {
+        //Introducimos la ruta de la carpeta que queremos listar todos los archivos
+        String ruta = "EjerciciosFichero/src";
+
+        File directorio = new File(ruta);
+
+        File [] archivos = directorio.listFiles();
+
+        if (archivos != null){
+            for (File archivo : archivos){
+                System.out.println(archivo.getName());
+            }
+        }
+
+    }
+}
